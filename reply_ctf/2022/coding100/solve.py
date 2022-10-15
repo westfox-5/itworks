fe23194fe4135822d65ef639ecc3f5faaa1e862c
Davide Volpe <volpe.davide.vd@gmail.com>
2022-10-15 11:54:30 +0200

    mannaggia a voi che avete fatto soluzioni multiple!

--- reply_ctf/2022/coding100/solve.py
+++ reply_ctf/2022/coding100/solve.py
@@ -6,6 +6,8 @@ FILENAME = "challenge.txt"
 NROWS = 45
 NCOLS = 45
 
+TARGET = "WHISPERING"
+
 GRID_LINE = "Grid:"
 WORDS_LINE = "Words:"
 MARKER = "#"
@@ -44,32 +46,93 @@ def parse_file():
             r += 1
 
 directions = [
-    [-1, 0], [1, 0], [0, 1], [0, -1],
-    [1, 1], [1, -1], [-1, -1], [-1, 1]]
+    ["UP", -1, 0], ["DOWN", 1, 0], ["RIGHT", 0, 1], ["LEFT", 0, -1],
+    ["DR", 1, 1], ["DL", 1, -1], ["UL", -1, -1], ["UR",-1, 1]]
+
+dirs = {
+    "UP": (-1,0),
+    "DOWN": (1,0),
+    "RIGHT": (0,1),
+    "LEFT": (0,-1),
+}
 
 def find_word_path_aux(grid, w, r, c):
     if grid[r][c] != w[0]:
         return (False, None)
-    
-    for i, j in directions:
+
+    possible_dir = {}
+
+    for dir, i, j in directions:
         rd, cd = r+i, c+j
-        flag = True
-        
-        path = []
-        path.append((r,c))
+        possible_dir[dir] = []
+
+        possible_dir[dir].append((r,c))
 
         for k in range(1, len(w)):
-            if 0<=rd<NROWS and 0<=cd<NCOLS and w[k] == grid[rd][cd]:
-                path.append((rd, cd))
+            if 0<=rd<NROWS and 0<=cd<NCOLS:
+                if w[k] == grid[rd][cd]:
+                    possible_dir[dir].append((rd, cd))
+                    rd, cd = rd+i, cd+j
+                else:
+                    break
 
-                rd += i
-                cd += j
-            else:
-                flag = (False, None)
-                break
-        if flag:
-            return (True,path)
+    for dir in possible_dir:
+        path = possible_dir[dir]
 
+        if len(path) == 0:
+            continue
+
+        if len(path) == len(w):
+            return (True, path)
+
+        possible_angles = []
+        if dir == "UP" or dir == "DOWN":
+            possible_angles.append((0, -1))
+            possible_angles.append((0, 1))
+        elif dir == "RIGHT" or dir == "LEFT":
+            possible_angles.append((1, 0))
+            possible_angles.append((-1, 0))
+        
+        if len(possible_angles) == 0:
+            continue
+
+        for idx, (rCURRENT, cCURRENT) in enumerate(path):
+            new_path = []
+            new_path.extend(path[:idx:])
+            new_path.append((rCURRENT,cCURRENT))
+    
+            for ai, aj in possible_angles:
+
+                rd = rCURRENT + ai
+                cd = cCURRENT + aj
+
+                # if w==TARGET and len(path) == 4:
+                #     print(new_path)
+
+                if 0<=rd<NROWS and 0<=cd<NCOLS:
+                    # if rd == 24 and cd == 37:
+                    #     print("HELLOU")
+
+                    # if len(path) == 4:
+                    #     print(f"w[idx+1]: {w[idx+1]}")
+                    #     print(f"({rd},{cd})\t: {grid[rd][cd]}")
+                    #     print((ai, aj))
+
+                    if w[idx+1] == grid[rd][cd]:
+                        new_path.append((rd, cd))
+                        flag = True
+
+                        rd, cd = rd+ai, cd+aj
+                        for k in range(idx+2, len(w)):
+                            if 0<=rd<NROWS and 0<=cd<NCOLS and w[k] == grid[rd][cd]:
+                                new_path.append((rd, cd))
+                                rd, cd = rd+ai, cd+aj
+                            else:
+                                flag = False
+                                break
+                        
+                        if flag:
+                            return (True, new_path)
     return (False, None)
 
 def mark_path_in_grid(grid, path):
@@ -86,7 +149,7 @@ def find_word_path(grid, w):
 
 def prettyprint_word_found(word, path):
     (i,j) = path[0]
-    print(f"({'%02s' % i},{'%02s' % j}): {word}")
+    print(f"({'%02s' % i},{'%02s' % j}): {word} - PATH: {path}")
 
 def prettyprint_grid():
     print()
@@ -96,15 +159,35 @@ def prettyprint_grid():
         print(f"{'%03s' % idx} [{' '.join('%02s' % i for i in row)}]")
     print()
 
-if __name__ == "__main__":
+
+def compute_password():
+    password = ""
+    for idx, row in enumerate(grid):
+        password += ''.join('%s' % i for i in row if i != MARKER)
     
+    return password.strip()
+
+if __name__ == "__main__":
+
     parse_file()
 
     prettyprint_grid()
+
+    not_found = []
     for word in words:
         ret, path = find_word_path(grid, word)
         if ret:
             mark_path_in_grid(grid, path)
             prettyprint_word_found(word, path)
+        else:
+            not_found.append(word)
 
+    if len(not_found) > 0:
+        print()
+        print("!!! MISSING WORDS !!!")
+        for w in not_found:
+            print(w)
+    
     prettyprint_grid()
+    
+    print(f"PASSWORD: {compute_password()}")
